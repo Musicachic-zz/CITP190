e3ead63458df44e81d21fd47b96ad5452c936f7c
Musicachic <Musicachic@gmail.com>
2013-08-28 21:02:16 -0400

    Here is my Project 1 for my Intro to Java class.

---
+++ src/CITP190.java
@@ -0,0 +1,28 @@
+/*            *             ====================================================                                                                                                            ***                      ====================================================
+*            ***            # Name: Teresa Potts
+*          *******          # Course Name: CITP 190
+*           *****           # Java Source File: Hello World.java
+*            ***            # Project Name: Project 1
+*             *             # Description: Here is my first Hello World program.
+*                           ====================================================
+*      *              *
+*     ***            ***
+*   *******        *******
+*    *****          *****
+*     ***            ***
+*      *              *
+*
+*
+*/
+package citp190;
+
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World");
+    }
+}
