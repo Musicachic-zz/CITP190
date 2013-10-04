d283631a9ce69951dbf8958a0c7389f28b246619
Musicachic <Musicachic@gmail.com>
2013-10-04 14:13:21 -0400

    Assignment 5-3 for class....

--- src/NumberGame/NumberGame.java
+++ src/NumberGame/NumberGame.java
@@ -17,21 +17,21 @@
 
 package NumberGame;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
-import java.util.Random;
-import java.text.*;
 
 public class NumberGame
 {
+    static final int MIN = 1;
+    static final int MAX = 101;
+
+
     public static void main (String[] args)
     {
 
-        int MIN = 1;
-        int MAX = 101;
-        int ACCUMULATOR = 0;
+        int accumulator = 0;
         String choice = "y";
-        while (choice.equalsIgnoreCase("y")) {
+        while (choice.equalsIgnoreCase("y"))
+        {
 
             //Welcome statement to the user.
             System.out.println("Welcome to the Guess Number Game");
@@ -43,50 +43,45 @@ public class NumberGame
             System.out.println();
 
             //Generate the computer's random guess.
-            double computerGuess = Math.random();
-            double comGuess = ((computerGuess * 100) + 1)%100;
-            int cg = (int) comGuess;
+            int cg = 1 + (int) (Math.random() * 100);
             System.out.println(cg);
 
             //Have the user enter guesses.
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter number: ");
-            int y = sc.nextInt();
+            int y = -1;
 
             while (y != cg)
 
             {
+
                 y = getInt(sc, y);
-                y = getIntWithinRange(sc, y, MIN, MAX);
 
                 if (y > cg + 10)
                 {
                     System.out.println("Way too high! Guess again.");
 
-                }
-                else if (y < cg)
+                } else if (y < cg)
                 {
                     System.out.println("Too low! Guess again.");
-                }
-
-                else
+                } else
                     System.out.println("Too high! Guess again.");
 
-                ACCUMULATOR += 1;
-
-                y = sc.nextInt();
+                accumulator += 1;
 
             }
-        if (ACCUMULATOR <= 3)
-        {
-            System.out.println("Great work! You are a mathematical wizard.");
-        }
-        else if (ACCUMULATOR > 7)
-        {
-            System.out.println("What took you so long? Maybe you should take some lessons.");
-        }
-        else
-            System.out.println("Not too bad! You've got some potential.");
+            if (accumulator <= 3)
+            {
+                System.out.println("Great work! You are a mathematical wizard.");
+            }
+            else if (accumulator > 7)
+            {
+                System.out.println("What took you so long? Maybe you should take some lessons.");
+            }
+            else
+            {
+                System.out.println("Not too bad! You've got some potential.");
+            }
 
             System.out.println("Would you like to play again? (y/n): ");
 
@@ -97,29 +92,21 @@ public class NumberGame
         System.out.println();
         System.out.println("Thanks for playing!");
     }
-    public static int getIntWithinRange (Scanner sc, int y, int min, int max){
-        boolean isValid = false;
-        while (!isValid)
+
+    public static boolean validateWithinRange(int y)
+    {
+        if (y < MIN)
         {
-            if (y < min)
-            {
-                System.out.println(
-                        "Error! Number must be greater than " + min + ".");
-                System.out.println("Enter another number: ");
-                y = sc.nextInt();
-            }
-            else if (y >= max)
-            {
-                System.out.println(
-                        "Error! Number must be less than " + max + ".");
-                System.out.println("Enter another number: ");
-                y = sc.nextInt();
-            }
-            else isValid = true;
-
-
+            System.out.println("Error! Number must be greater than " + MIN + ".");
+            return false;
         }
-        return y;
+        else if (y >= MAX)
+        {
+            System.out.println("Error! Number must be less than " + MAX + ".");
+            return false;
+        }
+        return true;
+
     }
 
     public static int getInt(Scanner sc, int y){
@@ -127,17 +114,20 @@ public class NumberGame
         boolean isValid = false;
         while (!isValid)
         {
-            if (sc.hasNextInt())
+            String raw_input = sc.nextLine();
+
+            try
             {
-                y = sc.nextInt();
-                isValid = true;
+                y = Integer.parseInt(raw_input);
+                if (validateWithinRange(y))
+                {
+                    isValid = true;
+                }
+
             }
-            else
+            catch(NumberFormatException e)
             {
-                sc.nextLine();
-                System.out.println(
-                        "Error! Invalid integer value. Try again.");
-                y = sc.nextInt();
+                System.out.println("Error! Invalid integer value. Try again.");
             }
 
 
