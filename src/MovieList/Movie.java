d521a4a993cd9a52d0bc0253c834142a04f95973
Musicachic <Musicachic@gmail.com>
2013-11-20 22:12:19 -0500

    Assignment 12-1 Starting it out...

---
+++ src/MovieList/Movie.java
@@ -0,0 +1,64 @@
+/*            *             ====================================================                                                                                                            ***                      ====================================================
+*            ***            # Name: Teresa Potts
+*          *******          # Course Name: CITP 190
+*           *****           # Java Source File: Movie.java
+*            ***            # Project Name: Workbook Assignment 12-1
+*             *             # Description: Here is my Movie List program.
+*                           ====================================================
+*      *              *
+*     ***            ***
+*   *******        *******
+*    *****          *****
+*     ***            ***
+*      *              *
+*
+*
+*/
+
+package MovieList;
+
+public class Movie implements Comparable
+{
+    private String title;
+    private String genre;
+
+
+    public Movie(String title, String genre)
+    {
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getGenre()
+    {
+        return genre;
+    }
+
+    public void setGenre(String genre)
+    {
+        this.genre = genre;
+    }
+
+    @Override
+    public int compareTo(Object o)
+    {
+        Movie m = (Movie) o;
+        int genreCompare = this.getGenre().compareTo(m.getGenre());
+        if (genreCompare != 0)
+            return genreCompare;
+        else
+            return(this.getGenre().compareToIgnoreCase(m.getGenre()));
+    }
+}
+
+
---
+++ src/MovieList/MovieApp.java
@@ -0,0 +1,67 @@
+/*            *             ====================================================                                                                                                            ***                      ====================================================
+*            ***            # Name: Teresa Potts
+*          *******          # Course Name: CITP 190
+*           *****           # Java Source File: MovieApp.java
+*            ***            # Project Name: Workbook Assignment 12-1
+*             *             # Description: Here is my Movie List program.
+*                           ====================================================
+*      *              *
+*     ***            ***
+*   *******        *******
+*    *****          *****
+*     ***            ***
+*      *              *
+*
+*
+*/
+
+package MovieList;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MovieApp {
+
+    public static void main (String args[])
+    {
+        System.out.println("Welcome to the Movie List Application.");
+        System.out.println("There are 100 movies on the list.");
+
+
+        Scanner sc = new Scanner(System.in);
+
+        String choice = "y";
+
+        ArrayList<Movie> MyListOfMovies = CreateArrayList();
+
+        System.out.println(MyListOfMovies.size());
+
+
+        while (choice.equalsIgnoreCase("y"))
+        {
+            System.out.println("There are four categories: animated, drama, horror, and scifi");
+            System.out.println();
+
+            String personMovieCategory = Validator.getRequiredString(sc, "What category are you interested in?");
+
+
+
+            choice = Validator.getRequiredString(sc, "Continue? (y/n): ");
+            System.out.println();
+
+        }
+    }
+
+    public static ArrayList<Movie> CreateArrayList()
+    {
+        Movie m;
+        ArrayList<Movie> CreatingArrayList = new ArrayList<>();
+        for (int i = 0; i <= 100; i++ )
+        {
+            m = MovieIO.getMovie(i);
+
+            CreatingArrayList.add(m);
+        }
+        return CreatingArrayList;
+    }
+}
